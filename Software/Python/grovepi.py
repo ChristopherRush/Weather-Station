6e49a59b8df8b03db191b63b32c0a3f0be06d62c
ChristopherRush <chris.uclan@gmail.com>
2016-06-10 18:50:08 +0100

    updated diameter

--- Software/Python/grovepi.py
+++ Software/Python/grovepi.py
@@ -209,7 +209,7 @@ def anenometerRead():
 	time.sleep(.2)
 	number = read_i2c_byte(address)
 	rpm = number * 60
-	mph = 0.22 * 3.14 * rpm * 60 / 5280
+	mph = 0.7 * 3.14 * rpm * 60 / 5280
 	#mph = 2.75 / 12 * 3.14 * number * 60 / 5280
 	#val = mph * 3.5
 	return mph
