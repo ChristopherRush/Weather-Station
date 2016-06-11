7adc49b5388e72e1291f8d6843585ae5c43c00b6
ChristopherRush <chris.uclan@gmail.com>
2016-06-11 15:42:07 +0100

    error

--- Projects/tmp36.py
+++ Projects/tmp36.py
@@ -9,6 +9,6 @@ while True:
     temp = grovepi.analogRead(sensor)
     volt = temp * 5.0 / 1024
     number = (volt - 0.5) * 100
-    number = round(t, 2)
+    number = round(number, 2)
     print (number)
     time.sleep(1)
