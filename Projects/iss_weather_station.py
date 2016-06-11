aa9f191a1527b19a11b4327b942a204c1805c61a
ChristopherRush <chris.uclan@gmail.com>
2016-06-11 16:38:04 +0100

    delay

--- Projects/iss_weather_station.py
+++ Projects/iss_weather_station.py
@@ -12,16 +12,18 @@ grovepi.pinMode(vane, "INPUT")
 
 while True:
     temp = grovepi.analogRead(sensor)
-    #volt = temp * 3.3 / 1024
-    #number = (volt - 0.5) * 100
-    #number = round(number, 2)
-    print (temp)
+    volt = temp * 5.0 / 1024
+    number = (volt - 0.5) * 100
+    number = round(number, 2)
+    print (number)
     #streamer.log("Temperature", number)
+    time.sleep(0.5)
 
     speed = grovepi.anenometerRead()
     streamer.log("Wind Speed", speed)
     print (speed)
 
+    time.sleep(0.5)
     direction = grovepi.analogRead(vane)
     #print direction
     if direction == 234:
