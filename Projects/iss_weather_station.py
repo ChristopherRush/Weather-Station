8910cea80ff01b5190dd49f18d94f24b8290bd9e
ChristopherRush <chris.uclan@gmail.com>
2016-06-11 14:18:37 +0100

    update

--- Projects/iss_weather_station.py
+++ Projects/iss_weather_station.py
@@ -10,8 +10,39 @@ while True:
     temp = grovepi.temp36(sensor)
     temp = round(temp,2)
     streamer.log("Temperature", temp)
-    print (temp)
+    #print (temp)
+
     speed = grovepi.anenometerRead()
     streamer.log("Wind Speed", speed)
+    #print (speed)
+
+    direction = grovepi.analogRead(vane)
+    print direction
+    if direction == 234:
+        streamer.log("Wind Direction", "North")
+        #print "North"
+    elif direction == 133:
+        streamer.log("Wind Direction", "North West")
+        #print "North West"
+    elif direction == 76:
+        streamer.log("Wind Direction", "West")
+        #print "West"
+    elif direction == 389:
+        streamer.log("Wind Direction", "South West")
+        #print "South West"
+    elif direction == 734:
+        streamer.log("Wind Direction", "South")
+        #print "South"
+    elif direction == 838:
+        streamer.log("Wind Direction", "South East")
+        #print "South East"
+    elif direction == 930:
+        streamer.log("Wind Direction", "East")
+        #print "East"
+    elif direction == 559:
+        streamer.log("Wind Direction", "North East")
+        #print "North East"
+
+        
     streamer.flush()
     time.sleep(30)
