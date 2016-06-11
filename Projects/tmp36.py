f3021b3da77ae76f4312c287d9b802a50f361550
ChristopherRush <chris.uclan@gmail.com>
2016-06-11 15:33:01 +0100

    up

--- Projects/tmp36.py
+++ Projects/tmp36.py
@@ -8,7 +8,7 @@ grovepi.pinMode(sensor, "INPUT")
 while True:
     temp = grovepi.analogRead(sensor)
     volts = temp * 5.0 / 1024
-	t = (volts - 0.5) * 100
+    t = (volts - 0.5) * 100
     t = round(t, 2)
-    print t
+    print (t)
     time.sleep(1)
