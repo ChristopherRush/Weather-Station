64a01d3ddee679285f2c7d6b0ae7ae411ae35bca
ChristopherRush <chris.uclan@gmail.com>
2016-06-11 14:12:04 +0100

    switch

--- Projects/wind_vane.py
+++ Projects/wind_vane.py
@@ -7,4 +7,20 @@ vane = 0
 while True:
     direction = grovepi.analogRead(vane)
     print direction
-    time.sleep(1)
+    if direction == 235:
+        print "North"
+    elif direction == 133:
+        print "North West"
+    elif direction == 76:
+        print "West"
+    elif direction == 389:
+        print "South West"
+    elif direction == 734:
+        print "South"
+    elif direction == 837:
+        print "South East"
+    elif direction == 929:
+        print "East"
+    elif direction == 559:
+        print "North East"
+    time.sleep(0.5)
