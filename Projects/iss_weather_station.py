e510bce84e98e5344df08135aee7e132180bd5c4
ChristopherRush <chris.uclan@gmail.com>
2016-06-11 13:19:34 +0100

    errors

--- Projects/iss_weather_station.py
+++ Projects/iss_weather_station.py
@@ -2,14 +2,13 @@ import time
 import grovepi
 from ISStreamer.Streamer import Streamer
 
-streamer = Streamer(bucket_name="weather", bucket_key="[63CXMKAEQVFR]", access_key="[NXNfXQSbMgvPPSqD0gEEr0zX3OHeam2r]")
+streamer = Streamer(bucket_name="weather", bucket_key="63CXMKAEQVFR", access_key="NXNfXQSbMgvPPSqD0gEEr0zX3OHeam2r")
 
 sensor = 1
 
 while True:
     temp = grovepi.temp36(sensor)
     temp = round(temp,2)
-    streamer.log("Temperature", "22")
+    streamer.log("Temperature", temp)
     print (temp)
-    streamer.flush()
     time.sleep(30)
