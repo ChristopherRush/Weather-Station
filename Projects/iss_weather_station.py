44571b245eca925893c1b60baa12f9003405f3e4
ChristopherRush <chris.uclan@gmail.com>
2016-09-27 17:34:44 +0100

    cxc

--- Projects/iss_weather_station.py
+++ Projects/iss_weather_station.py
@@ -35,7 +35,7 @@ while True:
     humid = grovepi.humidity()
     print (humid)
 
-    streamer.log(":thermometer: Humidity", humid + "%")
+    streamer.log(":thermometer: Humidity", humid)
     time.sleep(1)
 
 
